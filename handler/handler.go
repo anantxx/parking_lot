51af6c9d514793a0e4e99cd457a378f6e72576a4
ANANT HARAL <anant@qwentic.com>
2019-09-02 23:54:27 +0530

    Implemented File Handler functionality

--- handler/handler.go
+++ handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -12,49 +13,73 @@ import (
 	"github.com/parking_lot/service"
 )
 
-func HandleFile(fileName string) {
+func HandleFile(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-}
-
-func HandleCommand() {
-	fmt.Println("\n Enter Command:")
-	reader := bufio.NewReader(os.Stdin)
-	var command string
-	for {
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimRight(input, "\r\n")
-		arguments := []string{}
-		for _, s := range strings.Split(input, " ") {
-			if s != "" {
-				arguments = append(arguments, s)
-			}
-		}
-		if len(arguments) < 1 {
-			fmt.Println(ERR_NO_ARGS)
-			continue
-		}
-		command = arguments[0]
-		if strings.ToLower(command) == "exit" {
+	scanner := bufio.NewScanner(file)
+	var input string
+	for scanner.Scan() {
+		input = scanner.Text()
+		if false == processCommand(input) {
 			break
-		}
-		parkingModel := repository.GetInstance()
-		parkingRepo := repository.NewParkingRepository(parkingModel)
-		slotRepo := repository.NewSlotRepository()
-		parkingService := service.NewParkingService(parkingRepo, slotRepo)
-		res, err := ProcessCommand(parkingService, arguments)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(res)
 		}
 	}
 }
 
-func ProcessCommand(parkingService service.ServicesInstance, arguments []string) (response string, err error) {
+func HandleCommand() {
+	reader := bufio.NewReader(os.Stdin)
+	var (
+		input string
+		err   error
+	)
+	for {
+		input, err = reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		if false == processCommand(input) {
+			break
+		}
+	}
+}
+
+func processCommand(input string) bool {
+	input = strings.TrimRight(input, "\r\n")
+	arguments := []string{}
+	for _, s := range strings.Split(input, " ") {
+		if s != "" {
+			arguments = append(arguments, s)
+		}
+	}
+	if len(arguments) < 1 {
+		fmt.Println(ERR_NO_ARGS)
+		return true
+	}
+	if strings.ToLower(arguments[0]) == "exit" {
+		return false
+	}
+	parkingModel := repository.GetInstance()
+	parkingRepo := repository.NewParkingRepository(parkingModel)
+	slotRepo := repository.NewSlotRepository()
+	parkingService := service.NewParkingService(parkingRepo, slotRepo)
+	res, err := executeCommand(parkingService, arguments)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+	return true
+}
+
+func executeCommand(parkingService service.ServicesInstance, arguments []string) (response string, err error) {
 
 	switch arguments[0] {
 	case CREATE_PARKING_LOT:
-		if len(arguments) < 1 {
+		if len(arguments) < 2 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		totalSlot, err := strconv.Atoi(arguments[1])
@@ -63,13 +88,14 @@ func ProcessCommand(parkingService service.ServicesInstance, arguments []string)
 		}
 		return parkingService.InitializeLot(totalSlot)
 	case PARK:
-		if len(arguments) < 2 {
+		fmt.Println(len(arguments))
+		if len(arguments) < 3 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		return parkingService.AllocateSlot(arguments[1], arguments[2])
 
 	case LEAVE:
-		if len(arguments) < 1 {
+		if len(arguments) < 2 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		position, _ := strconv.Atoi(arguments[1])
@@ -78,17 +104,17 @@ func ProcessCommand(parkingService service.ServicesInstance, arguments []string)
 	case STATUS:
 		return parkingService.ShowStatus()
 	case REG_NUM_FOR_CAR_WITH_COL:
-		if len(arguments) < 1 {
+		if len(arguments) < 2 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		return parkingService.FindRegistationNosByColour(arguments[1])
 	case SLOT_NUM_FOR_CAR_WITH_COL:
-		if len(arguments) < 1 {
+		if len(arguments) < 2 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		return parkingService.FindAllocatedSlotByColour(arguments[1])
 	case SLOT_NUM_FOR_REG_NUM:
-		if len(arguments) < 1 {
+		if len(arguments) < 2 {
 			return "", ERR_INSUFFICIENT_ARGS
 		}
 		return parkingService.FindSlotByRegistationNo(arguments[1])
